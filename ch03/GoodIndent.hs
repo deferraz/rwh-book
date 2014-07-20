e0d2c3e51162983ba61eefa8b9400b8da4990843
Andre Ferraz <deferraz@terra.com.br>
2014-07-20 18:06:04 -0300

    examples of bad and good indentation and indentation of let and where expressions

---
+++ ch03/GoodIndent.hs
@@ -0,0 +1,8 @@
+-- file: ch03/GoodIndent.hs
+-- This is leftmost column
+
+  -- It's fine for top-evel declarations to start in any column...
+  firstGoodIndentation = 1
+
+  -- ... provided all subsequent declarations do, too!
+  secondGoodIndentation = 2
