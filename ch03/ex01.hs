9cff243773b8f5c8d7513bd5d30b81d34029514c
Andre Ferraz <deferraz@terra.com.br>
2014-08-02 15:07:56 -0300

    add ex01 from end of chapter

--- ch03/ex01.hs
+++ ch03/ex01.hs
@@ -1,3 +1,9 @@
-loList:: List a -> [a]
+toList:: List a -> [a]
 toList (Cons a Nil) = a:[]
 toList (Cons a b)   = a:toList b
+
+
+-- Exercise 01 from end of chapter
+myLength :: [a] -> Int
+myLength (x:xs) = 1 + myLength xs
+myLength []     = 0
